ff175eaf41ce84f721d71d44beff0a9393f0d290
Stephen O'Kane <sokratees9@googlemail.com>
2026-03-26 18:53:35 +0000

    - forgot to update version and name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "org.okane.voyagemapper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.1.1"
+        versionCode = 9
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 

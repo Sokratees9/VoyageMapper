21d06c76de4d0d27072f20739090031a34d02e52
Stephen O'Kane <sokratees9@googlemail.com>
2026-04-11 11:25:12 +0100

    - updated version code and name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "org.okane.voyagemapper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.1.2"
+        versionCode = 10
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
